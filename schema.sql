4ff9a5a28e52a5ce3a54f57d0378a36d763ff4e5
Niall Larson <nialldizzle@gmail.com>
2020-07-08 18:52:19 -0700

    updated db

--- schema.sql
+++ schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE customers
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(35) NOT NULL,
     last_name VARCHAR(35) NOT NULL,
-    phone_number INT(10) NOT NULL,
+    phone_number INT NOT NULL,
     PRIMARY KEY (id)
 );
