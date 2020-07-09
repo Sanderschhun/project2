ef8afb0d2c262068b44aec6b0ccbabbf1b0bd9b8
Niall Larson <nialldizzle@gmail.com>
2020-07-08 19:05:40 -0700

    more columns

--- schema.sql
+++ schema.sql
@@ -8,5 +8,7 @@ CREATE TABLE customers
     first_name VARCHAR(35) NOT NULL,
     last_name VARCHAR(35) NOT NULL,
     phone_number INT NOT NULL,
+    comments VARCHAR(2000),
+    goodVsEvil BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
