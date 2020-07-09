ea181e2ad55219939313e163690918c0629a7b49
wazhma shaiwayana <wazhma.shaiwayana@gmail.com>
2020-07-09 14:50:11 -0700

    updated folders, added the server.js and started most of the files

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var app = express();
+var port = process.env.port || 3000;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/routes.js");
+app.use(routes);
+
+app.listen(port, function() {
+    console.log(" lisening on port " + port);
+});
