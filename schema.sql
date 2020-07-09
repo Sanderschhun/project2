76815d3311a4517b24dbc6e194548d1b3faf1c4b
Niall Larson <nialldizzle@gmail.com>
2020-07-08 19:36:00 -0700

    updated column names

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE customers
     first_name VARCHAR(35) NOT NULL,
     last_name VARCHAR(35) NOT NULL,
     phone_number INT NOT NULL,
-    comments VARCHAR(2000),
-    goodVsEvil BOOLEAN NOT NULL,
+    reviews VARCHAR(2000),
+    thumbsUpDown BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
