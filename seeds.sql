9e03e33f4f949790228835c5e7a59287887dd70a
Niall Larson <nialldizzle@gmail.com>
2020-07-08 18:56:15 -0700

    our first customers

---
+++ seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO customers (first_name, last_name, phone_number) VALUES ("Karen", "Jabroni", 7704045555);
+INSERT INTO customers (first_name, last_name, phone_number) VALUES ("Mike", "Jones", 2813308004);
