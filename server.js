40698945f28bf9716e31342818ab7f5a1f19f19e
Sanderschhun <61896318+Sanderschhun@users.noreply.github.com>
2020-07-11 10:08:25 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
-var app = express();
-var port = process.env.port || 3000;
 
-app.use(express.static("public"));
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-var routes = require("./controllers/routes.js");
-app.use(routes);
-
-app.listen(port, function() {
-    console.log(" lisening on port " + port);
-});
