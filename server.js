dc7c57ed0a522b6e09a75b03a2c7f9022275a8dc
wazhma shaiwayana <wazhma.shaiwayana@gmail.com>
2020-07-11 11:16:35 -0700

    changed and added foler

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
-var db = require("./models");
+//var db = require("./models/customers.js");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,3 +13,6 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
+app.listen(port, function() {
+    console.log(" lisening on port " + port);
+});
