d4b528c1737503bca007664efe7d7626a0d0c13c
Niall Larson <nialldizzle@gmail.com>
2020-07-15 19:54:34 -0700

    validations

--- models/customers.js
+++ models/customers.js
@@ -1,10 +1,25 @@
 module.exports = function (sequelize, DataTypes) {
     var customer = sequelize.define("customer", {
         customerId: DataTypes.INTEGER,
-        customerName: DataTypes.STRING,
-        phoneNumber: DataTypes.INTEGER,
-        reviews: DataTypes.STRING,
-        thumbUpDown: DataTypes.BOOLEAN,
+        customerName:{
+            type: DataTypes.STRING,
+            len: [2,50],
+            isNull: false,
+        },
+        phoneNumber:{
+            type: DataTypes.INTEGER,
+            isNumeric: true,
+            isNull: false,
+        },
+        reviews:{
+            type: DataTypes.STRING,
+            len: [1,2000],
+            isNull: false,
+        },
+        thumbUpDown:{
+            type: DataTypes.BOOLEAN,
+            isNull: false,
+        },
     });
     return customer;
 };
